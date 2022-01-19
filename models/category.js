b8b8790f8af7c5e36d99e3e621bf7b2d8af6714f
rusty-reebs <rusty_reebs@tutanota.com>
2022-01-19 09:55:02 -0600

    Write category and made_in models

---
+++ models/category.js
@@ -0,0 +1,15 @@
+// category.js
+
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var CategorySchema = new Schema({
+  name: { type: String, required: true, maxlength: 100 },
+});
+
+CategorySchema.virtual("url").get(function () {
+  return "/category/" + this._id;
+});
+
+module.exports = mongoose.model("Category", CategorySchema);
---
+++ models/made_in.js
@@ -0,0 +1,15 @@
+// made_in.js
+
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var MadeInSchema = new Schema({
+  name: { type: String, required: true, maxlength: 100 },
+});
+
+MadeInSchema.virtual("url").get(function () {
+  return "/made_in/" + this._id;
+});
+
+module.exports = mongoose.model("Made In", MadeInSchema);
